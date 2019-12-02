cee605003c01aa5419163c82bdd9ed12bb46a964
hutumpyancha <upasana.ganguly1@louisiana.edu>
2019-12-02 14:34:32 -0600

    after if else and forloop

--- Bio595_Upasana_project.R
+++ Bio595_Upasana_project.R
@@ -9,6 +9,7 @@ library(robis)
 #help(package = "robis") #accessing OBIS data
 #?occurrence
 library(dplyr)
+library(plyr)
 spdata=occurrence("Pennatulacea")
 
 spdatasub= spdata[, c("scientificName", "maximumDepthInMeters", "decimalLatitude", "decimalLongitude")] #subset by column
@@ -30,72 +31,58 @@ V_data$species= str_split_fixed(V_data$scientificName, " ", 2)[,2]
 g <- str_sub(string = V_data$genus, start = 1, end = 1)
 V_data$genus.period <- str_c(g,".",sep = "")
 
-
-
-V = V_data %>% group_by(scientificName) %>% summarise(avg.depth = mean( maximumDepthInMeters, na.rm = T))
-
-#V_data$species= sub("", "sp.", V_data$species)
-
 V_data$species[V_data$species==""]<-"sp."
 
 V_data$genus.species= paste(V_data$genus, "", V_data$species)
 V_data$genus.sp= paste(V_data$genus.period, "", V_data$species)
 
+V_data1= subset(V_data, genus != "Pennatulacea")
+
+V = V_data1 %>% group_by(scientificName) %>% summarise(avg.depth = mean( maximumDepthInMeters, na.rm = T))
+
+
+
 
 taxon.fc <- function(x){
+  
+  if(V_data$species[i] == "sp."){
+    taxon = V_data1$genus.species[i]
+    
 
-  if(str_detect(string = "species", pattern = "sp.")){
-    taxon = V_data$genus.sp
     
-    
-  } else if(str_detect(string = "species", pattern != ("sp."))){
-    taxon = V_data$genus.species
-  } else {}
+  } else {taxon = V_data1$genus.sp[i]}
   
   return(taxon)
   
 }
 
-taxon.fc1 <- function(x){
-  
-  if(V_data$species == "sp."){
-    taxon = V_data$genus.sp
-    
-    
-  } else if(V_data$species != "sp."){
-    taxon = V_data$genus.species
-  } else {}
-  
-  return(taxon)
-  
-}
-
-
-V_data$taxon = NA
-for(i in 1:nrow(V_data)){
-  V_data[i,]$taxon <- taxon.fc1(x = V_data[i,])
+i=2
+V_data1$taxon = NA
+for(i in 1:nrow(V_data1)){
+  V_data1$taxon[i] <- taxon.fc(x = V_data1[i,])
 }
 
 
 
-p= ggplot(V_data, aes(scientificName)) + geom_histogram(stat="count") + theme_classic( base_size = 13)
+p= ggplot(V_data1, aes(genus, fill=genus)) + geom_histogram(stat="count") + theme_classic( base_size = 13)
+plot(p)
 
 dir.create("V_figures")
-ddply(.data = V_data, .variables = c("genus"), function(x){
+ddply(.data = V_data1, .variables = "genus", function(x){
   
-  t <- unique(x$scientificName)
+  t = unique(x$genus)
   
-  plot1 <- ggplot(anthoptilum_data1, aes(x=Name, y=maximumDepthInMeters, fill=Name) ) +
-    geom_boxplot() + scale_y_reverse(lim=c(4200,0),breaks=seq(0,4200,200),expand = c(0, 0)) +
+  pl = ggplot(V_data1, aes(x=taxon, y=maximumDepthInMeters, fill=taxon) ) +
+    geom_boxplot() + scale_y_reverse(lim=c(6000,0),breaks=seq(0,6000,200),expand = c(0, 0)) +
     scale_x_discrete(position="top") + theme_classic( base_size = 13) + 
     theme(axis.text.x = element_text(face="italic"), legend.position = "none") + 
     labs(title="Depth profile", x="",y="Depth m")
   
   ggsave(filename = paste0('V_figures/',t,'.png'),
-         plot = plot1, width = 4, height = 3, units = 'in',
-         dpi = 300)
+         plot = pl, width = 4, height = 3, units = 'in',
+         dpi = 600)
   
-}, .inform = T, .progress = "text")
+},  .progress = "text")
 
 
 
